cb602f748a1cf32b5d078cbc4398c649eca5381b
ckie <git-525ff67@ckie.dev>
2020-12-16 05:59:50 +0200

    add i3 to home-manager, fix picom+slock and a bit more

---
+++ modules/home/i3.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, ... }: {
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = {
+      gaps = {
+        inner = 10;
+        outer = 0;
+        smartGaps = true;
+      };
+      window = {
+        border = 0;
+        titlebar = false;
+      };
+      keybindings = with {
+        modifier = config.xsession.windowManager.i3.config.modifier;
+        spotifyWorkspace = "Spf";
+        locker =
+          "/run/wrappers/bin/slock"; # slock uses security.wrappers for setuid
+      };
+        lib.mkOptionDefault {
+          # brightness & audio Fn keys
+          "XF86MonBrightnessUp" = ''
+            exec "${pkgs.brightnessctl}/bin/brightnessctl set +5% && pkill -RTMIN+12 i3blocks"'';
+          "XF86MonBrightnessDown" = ''
+            exec "${pkgs.brightnessctl}/bin/brightnessctl set 5%- && pkill -RTMIN+12 i3blocks"'';
+          "XF86AudioMute" = ''
+            exec "${pkgs.alsaUtils}/bin/amixer -D pulse set Master 1+ toggle && pkill -RTMIN+2 i3blocks"'';
+          "XF86AudioRaiseVolume" = ''
+            exec "${pkgs.alsaUtils}/bin/amixer -D pulse sset Master 5%+ && pkill -RTMIN+2 i3blocks"'';
+          "XF86AudioLowerVolume" = ''
+            exec "${pkgs.alsaUtils}/bin/amixer -D pulse sset Master 5%- && pkill -RTMIN+2 i3blocks"'';
+          # meta
+          "${modifier}+Shift+f" = "floating toggle";
+          "--release ${modifier}+l" = "exec ${locker}";
+          "--release ${modifier}+Shift+l" = "exec ${locker} systemctl suspend";
+
+          "--release ${modifier}+End" = "exec ${./i3-scripts/screenshot}";
+          "--release ${modifier}+Shift+Pause" =
+            "exec ${./i3-scripts/screenshot}";
+
+          # spotify's house
+          "${modifier}+Shift+w" =
+            "move container to workspace ${spotifyWorkspace}";
+          "${modifier}+w" = "workspace ${spotifyWorkspace}";
+        };
+      fonts = [ "Hack 9" ];
+      modifier = "Mod4"; # super key
+      terminal = "${pkgs.kitty}/bin/kitty";
+      menu = "${pkgs.rofi}/bin/rofi -show drun";
+    };
+  };
+}
