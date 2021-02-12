8b7893d12019751a658e99d7763d0f3b773bb5e8
ckie <git-525ff67@ckie.dev>
2021-02-12 14:56:33 +0200

    move pookieix into folder and add udev rule for vchiq

--- hosts/pookieix/default.nix
+++ hosts/pookieix/default.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }: {
-  imports = [
-    /etc/nixos/hardware-configuration.nix
-    ../modules/base.nix
-    ../modules/home.nix
-  ];
+  imports = [ ./hardware.nix ../../modules/base.nix ../../modules/home.nix ];
+
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_rpi4;
 
   networking.hostName = "pookieix";
 
@@ -11,30 +11,14 @@
     enable = true;
     port = 5000;
   };
+  networking.firewall.allowedTCPPorts =
+    [ 5000 ]; # this is just weird iptables stuff
   users.users.octoprint.extraGroups = [ "dialout" ];
   networking.firewall.extraCommands = ''
     iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 5000
   '';
 
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
-  # Mainline doesn't work yet
-  boot.kernelPackages = pkgs.linuxPackages_rpi4;
-
-  # ttyAMA0 is the serial console broken out to the GPIO
-  boot.kernelParams = [
-    "8250.nr_uarts=1" # may be required only when using u-boot
-    "console=ttyAMA0,115200"
-    "console=tty1"
-  ];
-
-  # Required for the Wireless firmware
   hardware.enableRedistributableFirmware = true;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 5000 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.wireless.enable = false;
   networking.networkmanager.enable = true;
 
