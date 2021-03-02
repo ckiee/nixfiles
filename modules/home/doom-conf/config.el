2c41240976a010ad53f1681d86ae0149a3a77768
ckie <git-525ff67@ckie.dev>
2021-03-02 10:40:56 +0200

    things

--- modules/home/doom-conf/config.el
+++ modules/home/doom-conf/config.el
@@ -55,3 +55,9 @@
 (which-key-mode)
 ;; (global-wakatime-mode)
 (map! :leader "f P" (cmd! (doom-project-browse "~/git/nixfiles/modules/home/doom-conf/")))
+(defun count-buffers (&optional display-anyway)
+  "Display or return the number of buffers."
+  (interactive)
+  (let ((buf-count (length (buffer-list))))
+    (if (or (interactive-p) display-anyway)
+    (message "%d buffers in this Emacs" buf-count)) buf-count))
