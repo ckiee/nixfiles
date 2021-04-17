71667476bbedcb94c150b0f4f9a49ea5eefb4da7
Ron B <me@ronthecookie.me>
2021-04-17 22:55:52 +0300

    port keyboard

--- legacy/home/graphical/layout.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  xsession.windowManager.i3.config.startup = [{
-    command = "${pkgs.xorg.xmodmap}/bin/xmodmap ${./xmodmap-layout}";
-    notification = false;
-  }];
-
-  home.keyboard.layout = "us,il";
-  home.keyboard.options = [ "grp:win_space_toggle" ];
-}
