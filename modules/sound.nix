8c71bf8cf1555fe66233f87d06bb8b66c365f148
ckie <git-525ff67@ckie.dev>
2021-05-12 14:11:20 +0300

    new pulse config

--- modules/sound.nix
+++ modules/sound.nix
@@ -10,15 +10,26 @@ in with lib; {
 
   config = (mkIf cfg.enable {
     sound.enable = true;
-    hardware.pulseaudio.enable = true;
-    hardware.pulseaudio.daemon.config = mkIf cfg.lowLatency {
-      "high-priority" = "yes";
-      "nice-level" = "-15";
-      "realtime-scheduling" = "yes";
-      "realtime-priority" = "50";
-      "resample-method" = "speex-float-0";
-      "default-fragments" = "2";
-      "default-fragment-size-msec" = "2";
-    };
+    security.rtkit.enable = mkDefault true;
+    hardware.pulseaudio = mkMerge [
+      (mkIf cfg.lowLatency {
+        configFile = ../ext/default.pa;
+        daemon.config = {
+          "high-priority" = "yes";
+          "nice-level" = "-15";
+          "realtime-scheduling" = "yes";
+          "realtime-priority" = "50";
+          "resample-method" = "speex-float-0";
+          "default-fragments" = "2";
+          "default-fragment-size-msec" = "4";
+
+          "default-sample-format" = "s32le";
+          "default-sample-rate" = "48000";
+          "alternate-sample-rate" = "48000";
+          "default-sample-channels" = "2";
+        };
+      })
+      { enable = true; }
+    ];
   });
 }
