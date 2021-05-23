32aa14dcd68e0e2d4e893c3749b5a60f9b0ef8d7
ckie <git-525ff67@ckie.dev>
2021-05-23 15:33:30 +0300

    replace nixpkgs.config with overlays

--- modules/slock.nix
+++ modules/slock.nix
@@ -9,23 +9,25 @@ in with lib; {
   config = mkIf cfg.enable {
     programs.slock.enable = true;
 
-    nixpkgs.config.packageOverrides = pkgs: {
-      slock = pkgs.slock.override {
-        conf = ''
-          /* user and group to drop privileges to */
-          static const char *user  = "nobody";
-          static const char *group = "nogroup";
+    nixpkgs.overlays = [
+      (self: super: {
+        slock = super.slock.override {
+          conf = ''
+            /* user and group to drop privileges to */
+            static const char *user  = "nobody";
+            static const char *group = "nogroup";
 
-          static const char *colorname[NUMCOLS] = {
-          	[INIT] =   "#1b1720",   /* after initialization */
-          	[INPUT] =  "#27212e",   /* during input */
-          	[FAILED] = "#ffe261",   /* wrong password */
-          };
+            static const char *colorname[NUMCOLS] = {
+            	[INIT] =   "#1b1720",   /* after initialization */
+            	[INPUT] =  "#27212e",   /* during input */
+            	[FAILED] = "#ffe261",   /* wrong password */
+            };
 
-          /* treat a cleared input like a wrong password (color) */
-          static const int failonclear = 0;'';
-      };
-    };
+            /* treat a cleared input like a wrong password (color) */
+            static const int failonclear = 0;'';
+        };
+      })
+    ];
 
     # TODO set the i3 locker here instead of in xsession
   };
