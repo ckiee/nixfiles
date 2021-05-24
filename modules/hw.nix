40291d218c330f2818ce815a41cf6d762cb612a0
ckie <git-525ff67@ckie.dev>
2021-05-24 08:36:24 +0300

    hw: new t480s stuff

--- modules/hw.nix
+++ modules/hw.nix
@@ -10,29 +10,40 @@ in with lib; {
   };
 
   # We need to do this after the libinput Xorg config
-  config.services.xserver.config = mkIf cfg.t480s.enable (mkAfter ''
-    Section "InputClass"
-      Identifier "Set NatrualScrolling for TrackPoint"
-      Driver "libinput"
-      MatchIsPointer "on"
-      Option "NaturalScrolling" "off"
-      Option "Tapping" "off"
-    EndSection
-  '');
-  config.services.throttled = mkIf cfg.t480s.undervolt {
-    enable = true;
-    # this overrides the default t480s stuff so just temp:
-    extraConfig = ''
-      [UNDERVOLT]
-      # CPU core voltage offset (mV)
-      CORE: -60
-      # Integrated GPU voltage offset (mV)
-      GPU: -85
-      # CPU cache voltage offset (mV)
-      CACHE: -105
-      # System Agent voltage offset (mV)
-      UNCORE: -85
-      # Analog I/O voltage offset (mV)
-      ANALOGIO: 0'';
-  };
+  config = mkMerge [
+    (mkIf cfg.t480s.enable {
+      services.xserver.config = (mkAfter ''
+        Section "InputClass"
+          Identifier "Set NatrualScrolling for TrackPoint"
+          Driver "libinput"
+          MatchIsPointer "on"
+          Option "NaturalScrolling" "off"
+          Option "Tapping" "off"
+        EndSection
+      '');
+
+      services.logind.extraConfig = ''
+        HandlePowerKey=ignore
+      '';
+    })
+    (mkIf cfg.t480s.undervolt {
+      services.throttled = {
+        enable = true;
+        # this overrides the default t480s stuff so just temp:
+        extraConfig = ''
+          [UNDERVOLT]
+          # CPU core voltage offset (mV)
+          CORE: -60
+          # Integrated GPU voltage offset (mV)
+          GPU: -85
+          # CPU cache voltage offset (mV)
+          CACHE: -105
+          # System Agent voltage offset (mV)
+          UNCORE: -85
+          # Analog I/O voltage offset (mV)
+          ANALOGIO: 0'';
+      };
+    })
+  ];
+
 }
