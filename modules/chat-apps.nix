e2f708e1af38c677e5be9441e9b1547b1c255ac6
ckie <git-525ff67@ckie.dev>
2021-05-28 11:59:49 +0300

    update dependencies

--- modules/chat-apps.nix
+++ modules/chat-apps.nix
@@ -10,17 +10,17 @@ in with lib; {
     environment.systemPackages = with pkgs; [ discord discord-ptb weechat ];
 
     # Bump discord without bumping nixpkgs
-    nixpkgs.overlays = [
-      (self: super: {
-        discord = super.discord.overrideAttrs (_: {
-          src = builtins.fetchTarball
-            "https://dl.discordapp.net/apps/linux/0.0.15/discord-0.0.15.tar.gz";
-        });
-        discord-ptb = super.discord-ptb.overrideAttrs (_: {
-          src = builtins.fetchTarball
-            "https://dl-ptb.discordapp.net/apps/linux/0.0.25/discord-ptb-0.0.25.tar.gz";
-        });
-      })
-    ];
+    # nixpkgs.overlays = [
+    #   (self: super: {
+    #     discord = super.discord.overrideAttrs (_: {
+    #       src = builtins.fetchTarball
+    #         "https://dl.discordapp.net/apps/linux/0.0.15/discord-0.0.15.tar.gz";
+    #     });
+    #     discord-ptb = super.discord-ptb.overrideAttrs (_: {
+    #       src = builtins.fetchTarball
+    #         "https://dl-ptb.discordapp.net/apps/linux/0.0.25/discord-ptb-0.0.25.tar.gz";
+    #     });
+    #   })
+    # ];
   };
 }
