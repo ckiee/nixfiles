2805431a88a3185432c48ca3619047923b79c8ff
ckie <git-525ff67@ckie.dev>
2021-06-02 22:33:17 +0300

    rtc-files: init

---
+++ modules/services/rtc-files.nix
@@ -0,0 +1,44 @@
+{ lib, config, pkgs, ... }:
+
+let cfg = config.cookie.services.rtc-files;
+
+in with lib; {
+  options.cookie.services.rtc-files = {
+    enable = mkEnableOption "Enables rtc-files service";
+    host = mkOption {
+      type = types.str;
+      default = "devel.i.ronthecookie.me";
+      description = "the host. wow.";
+      example = "i.ronthecookie.me";
+    };
+    redirect = mkOption {
+      type = types.str;
+      default = "ronthecookie.me";
+      description = "host to redirect / to";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [{
+      assertion = config.cookie.services.nginx.enable;
+      message = "nginx required";
+    }];
+
+    system.activationScripts = {
+      rtc-files-mkdir.text = ''
+        mkdir -p /cookie/rtc-files || true
+
+        # nginx writes, ron reads
+        chmod 740 /cookie/rtc-files
+        chown ron:nginx /cookie/rtc-files
+      '';
+    };
+
+    services.nginx = {
+      virtualHosts."${cfg.host}" = {
+        locations."/" = { root = "/cookie/rtc-files"; };
+        extraConfig = "rewrite ^/$ $scheme://${cfg.redirect} permanent;";
+      };
+    };
+  };
+}
