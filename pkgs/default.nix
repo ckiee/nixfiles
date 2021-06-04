20ec4b633f5a44f802ca38d68038bad1ddd7c2f2
Ron B <me@ronthecookie.me>
2021-06-04 15:05:23 +0300

    owo-bot: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
 let sources = import ../nix/sources.nix;
 in {
   comicfury-discord-webhook = import sources.comicfury-discord-webhook;
+  owo-bot = import sources.owo-bot;
   ronthecookieme = pkgs.callPackage ./ronthecookieme { };
 }
