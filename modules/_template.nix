8fedb66a60692e2df5d1501dc358fe63597a4db0
Ron B <me@ronthecookie.me>
2021-06-11 11:57:24 +0300

    _template: simplify

--- modules/_template.nix
+++ modules/_template.nix
@@ -4,7 +4,7 @@ let cfg = config.cookie.foo;
 
 in with lib; {
   options.cookie.foo = {
-    enable = mkEnableOption "Enables magical foo service";
+    enable = mkEnableOption "Enables foo";
   };
 
   config = mkIf cfg.enable { };
