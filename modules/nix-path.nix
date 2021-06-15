dc0d2b6ac8cc3b77733542a52fa27eae3f819360
ckie <git-525ff67@ckie.dev>
2021-06-15 12:18:38 +0300

    nix-path: oops we have secrets now

--- modules/nix-path.nix
+++ modules/nix-path.nix
@@ -1,21 +1,15 @@
 { lib, config, pkgs, ... }:
 
-let
-  cfg = config.cookie.nix-path;
-  nixfiles = ./..; # TODO: remove when adding secrets
+let cfg = config.cookie.nix-path;
 in with lib; {
   options.cookie.nix-path = {
     enable = mkEnableOption "Enables usage of the pinned nixpkgs in $NIX_PATH";
   };
 
   config = mkIf cfg.enable {
-    nix.nixPath = map (x: "${x}=/run/current-system/sw/${x}") [
-      "nixpkgs"
-      "nixos-config" # This actually should point to a path in hosts but it seems just going with the default.nix makes things work...somehow.
-    ];
+    nix.nixPath = map (x: "${x}=/run/current-system/sw/${x}") [ "nixpkgs" ];
     environment.extraSetup = ''
       ln -s ${pkgs.path} $out/nixpkgs
-      ln -s ${nixfiles} $out/nixos-config
     '';
   };
 }
