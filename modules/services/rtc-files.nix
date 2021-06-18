42505b2cb4867ee893479af5b987007bd4662307
ckie <git-525ff67@ckie.dev>
2021-06-18 11:23:13 +0300

    rtc-files: support ckie.dev

--- modules/services/rtc-files.nix
+++ modules/services/rtc-files.nix
@@ -5,16 +5,15 @@ let cfg = config.cookie.services.rtc-files;
 in with lib; {
   options.cookie.services.rtc-files = {
     enable = mkEnableOption "Enables rtc-files service";
-    host = mkOption {
-      type = types.str;
-      default = "rtc-files.localhost";
-      description = "the host. wow.";
-      example = "i.ronthecookie.me";
-    };
-    redirect = mkOption {
+    old-fqdn = mkOption {
       type = types.str;
       default = "ronthecookie.me";
-      description = "host to redirect / to";
+      description = "old fqdn";
+    };
+    new-fqdn = mkOption {
+      type = types.str;
+      default = "ckie.dev";
+      description = "new fqdn";
     };
     folder = mkOption {
       type = types.str;
@@ -28,24 +27,38 @@ in with lib; {
 
     system.activationScripts = {
       rtc-files-mkdir.text = ''
-        mkdir -p ${cfg.folder} || true
+        mkdir -p ${cfg.folder}/ckiedev || true
 
-        chmod 750 ${cfg.folder}
-        chmod g+s ${cfg.folder}
-        chown ckie:nginx ${cfg.folder}
+        chmod -R 750 ${cfg.folder}
+        chmod -R g+s ${cfg.folder}
+        chown -R ckie:nginx ${cfg.folder}
       '';
     };
 
     services.nginx = {
-      virtualHosts."${cfg.host}" = {
-        locations."/" = { root = cfg.folder; };
-        extraConfig = ''
-          rewrite ^/$ $scheme://${cfg.redirect} permanent;
-          access_log /var/log/nginx/rtc-files.access.log;
-        '';
+      virtualHosts = {
+        "i.${cfg.old-fqdn}" = {
+          locations."/" = { root = cfg.folder; };
+          extraConfig = ''
+            rewrite ^/$ $scheme://${cfg.old-fqdn} permanent;
+            # Redirect everything under /ckiedev to the new host
+            rewrite ^/ckiedev/(.*)$ $scheme://i.${cfg.new-fqdn}/$1 permanent;
+
+            access_log /var/log/nginx/rtc-files.access.log;
+          '';
+        };
+        "i.${cfg.new-fqdn}" = {
+          locations."/" = { root = cfg.folder + "/ckiedev"; };
+          extraConfig = ''
+            # Redirect ckie.dev root url (/) to new FQDN
+            rewrite ^/$ $scheme://${cfg.new-fqdn} permanent;
+
+            access_log /var/log/nginx/ckie-files.access.log;
+          '';
+        };
       };
     };
 
-    cookie.services.prometheus.nginx-vhosts = [ "rtc-files" ];
+    cookie.services.prometheus.nginx-vhosts = [ "ckie-files" "rtc-files" ];
   };
 }
