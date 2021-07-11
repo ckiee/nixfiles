0d72ecc1db106976f5a7679b1610f83bc2c43e3a
ckie <git-525ff67@ckie.dev>
2021-07-11 23:15:33 +0300

    murder the old name a bit more

--- pkgs/ronthecookieme/default.nix
+++ pkgs/ronthecookieme/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "git";
 
   src = fetchFromGitHub {
-    owner = "ronthecookie";
+    owner = "ckiee";
     repo = "ronthecookie.me";
     rev = "d11223321a8f0f1c6b9cca221b114f00e339c683";
     sha256 = "014pcc01d87sylf8g8x58aar31jrb3qwb8qfs1v7v129v3ikr2ys";
