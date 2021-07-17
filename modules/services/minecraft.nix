f424689119b0c35b498fbbdcea6f96aa3773d7ed
ckie <git-525ff67@ckie.dev>
2021-07-17 22:18:57 +0300

    minecraft: switch to bindfs

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -31,20 +31,13 @@ in with lib; {
         "-Xms3G -Xmx3G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
     };
 
-    users = {
-      groups.minecraft = { };
-      users = {
-        ckie.extraGroups =
-          [ "minecraft" ]; # I need to have read-write for /var/lib/minecraft
-        minecraft.group = "minecraft";
-      };
-    };
     # We need a separate unit so we can use root privileges for this
     systemd.services.minecraft-server-perms = {
       description = "Setup permissions for /var/lib/minecraft";
-      script = ''
-        ${pkgs.coreutils}/bin/chmod -R 2777 /var/lib/minecraft
-        ${pkgs.coreutils}/bin/chown -R minecraft:minecraft /var/lib/minecraft
+      script = "true";
+      postStart = ''
+        ${pkgs.coreutils}/bin/mkdir /home/ckie/minecraft
+        ${pkgs.bindfs}/bin/bindfs --create-for-user=minecraft --create-with-perms=0700 -u ckie -g users -p 0600,u+X /var/lib/minecraft /home/ckie/minecraft
       '';
       wantedBy = [ "minecraft-server.service" ];
     };
