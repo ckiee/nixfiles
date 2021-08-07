535edb760e746acaf02b350a03a40434f89cd530
ckie <git-525ff67@ckie.dev>
2021-08-07 05:23:37 +0300

    matrix: conditionally enable discord appservice

--- modules/services/matrix/discord.nix
+++ modules/services/matrix/discord.nix
@@ -4,35 +4,39 @@ with lib;
 
 let cfg = config.cookie.services.matrix;
 in {
-  cookie.secrets.matrix-appservice-discord = {
-    source = ../../../secrets/matrix-appservice-discord.env;
-    dest = "/var/run/matrix-appservice-discord.env";
-    owner = "root";
-    group = "root";
-    permissions = "0400";
-  };
+  config = mkIf cfg.enable {
 
-  services.matrix-appservice-discord = {
-    enable = true;
-    serviceDependencies = [ "matrix-synapse.service" ];
-    environmentFile = /. + config.cookie.secrets.matrix-appservice-discord.dest;
-    settings.bridge = {
-      domain = cfg.host;
-      homeserverUrl = "https://${cfg.serviceHost}";
+    cookie.secrets.matrix-appservice-discord = {
+      source = ../../../secrets/matrix-appservice-discord.env;
+      dest = "/var/run/matrix-appservice-discord.env";
+      owner = "root";
+      group = "root";
+      permissions = "0400";
     };
-  };
 
-  services.matrix-synapse.app_service_config_files =
-    [ "/var/lib/matrix-synapse/discord/discord-registration.yaml" ];
+    services.matrix-appservice-discord = {
+      enable = true;
+      serviceDependencies = [ "matrix-synapse.service" ];
+      environmentFile = /.
+        + config.cookie.secrets.matrix-appservice-discord.dest;
+      settings.bridge = {
+        domain = cfg.host;
+        homeserverUrl = "https://${cfg.serviceHost}";
+      };
+    };
 
-  systemd.services.matrix-discord-perms = {
-    description = "Setup bindfs for /var/lib/matrix-synapse/discord";
-    wantedBy = [ "matrix-synapse.service" ];
-    serviceConfig.Type = "forking";
+    services.matrix-synapse.app_service_config_files =
+      [ "/var/lib/matrix-synapse/discord/discord-registration.yaml" ];
 
-    preStart =
-      "${pkgs.coreutils}/bin/mkdir /var/lib/matrix-synapse/discord || true";
-    script =
-      "${pkgs.bindfs}/bin/bindfs -u matrix-synapse -g matrix-synapse -p 0400,u+X /var/lib/matrix-appservice-discord /var/lib/matrix-synapse/discord";
+    systemd.services.matrix-discord-perms = {
+      description = "Setup bindfs for /var/lib/matrix-synapse/discord";
+      wantedBy = [ "matrix-synapse.service" ];
+      serviceConfig.Type = "forking";
+
+      preStart =
+        "${pkgs.coreutils}/bin/mkdir /var/lib/matrix-synapse/discord || true";
+      script =
+        "${pkgs.bindfs}/bin/bindfs -u matrix-synapse -g matrix-synapse -p 0400,u+X /var/lib/matrix-appservice-discord /var/lib/matrix-synapse/discord";
+    };
   };
 }
