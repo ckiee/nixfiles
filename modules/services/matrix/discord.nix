490144803cfa1ba50af37c09d468e39257541d3c
ckie <git-525ff67@ckie.dev>
2021-08-11 23:49:37 +0300

    services/matrix: convert discord to the new module

--- modules/services/matrix/discord.nix
+++ modules/services/matrix/discord.nix
@@ -5,7 +5,6 @@ with lib;
 let cfg = config.cookie.services.matrix;
 in {
   config = mkIf cfg.enable {
-
     cookie.secrets.matrix-appservice-discord = {
       source = ../../../secrets/matrix-appservice-discord.env;
       dest = "/var/run/matrix-appservice-discord.env";
@@ -28,15 +27,12 @@ in {
     services.matrix-synapse.app_service_config_files =
       [ "/var/lib/matrix-synapse/discord/discord-registration.yaml" ];
 
-    systemd.services.matrix-discord-perms = {
-      description = "Setup bindfs for /var/lib/matrix-synapse/discord";
+    cookie.bindfs.matrix-appservice-discord = {
+      source = "/var/lib/matrix-appservice-discord";
+      dest = "/var/lib/matrix-synapse/discord";
+      overlay = false;
+      args = "-u matrix-synapse -g matrix-synapse -p 0400,u+X";
       wantedBy = [ "matrix-synapse.service" ];
-      serviceConfig.Type = "forking";
-
-      preStart =
-        "${pkgs.coreutils}/bin/mkdir /var/lib/matrix-synapse/discord || true";
-      script =
-        "${pkgs.bindfs}/bin/bindfs -u matrix-synapse -g matrix-synapse -p 0400,u+X /var/lib/matrix-appservice-discord /var/lib/matrix-synapse/discord";
     };
   };
 }
