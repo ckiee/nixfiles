f8c4e3845e98ce598ab2fe8ec883fbdca0ba3f12
ckie <git-525ff67@ckie.dev>
2021-09-07 23:42:33 +0300

    services/scanner: wip

--- hosts/drapion/default.nix
+++ hosts/drapion/default.nix
@@ -14,6 +14,7 @@ with lib;
     services = {
       avahi.enable = true;
       isp-troll.enable = true;
+      scanner.enableServer = true;
       coredns = {
         enable = true;
         openFirewall = true;
