9604695f28bf652a092f92b613d66608733a000f
ckie <git-525ff67@ckie.dev>
2021-09-17 20:15:23 +0300

    metadata: switch to readOnly,default

--- modules/metadata.nix
+++ modules/metadata.nix
@@ -4,8 +4,10 @@ let cfg = config.cookie.metadata;
 
 in with lib; {
   options.cookie.metadata = {
-    raw = mkOption { description = "the raw metadata"; };
+    raw = mkOption {
+      description = "the raw metadata";
+      readOnly = true;
+      default = importTOML ../ext/metadata.toml;
+    };
   };
-
-  config.cookie.metadata = { raw = importTOML ../ext/metadata.toml; };
 }
