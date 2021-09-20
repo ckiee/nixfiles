293d6624c2d73222a1d11cb1bfc3631aefa0d0d9
ckie <git-525ff67@ckie.dev>
2021-09-20 22:22:39 +0300

    hardware: move t480s-sdcard to hosts/thonkcookie

--- modules/hw.nix
+++ modules/hw.nix
@@ -31,14 +31,6 @@ in with lib; {
       services.logind.extraConfig = ''
         HandlePowerKey=ignore
       '';
-
-      # The SD-card reader is power hungry. Kill it!
-      # https://wiki.archlinux.org/title/Lenovo_ThinkPad_T480s#SD_card_reader
-      systemd.services."t480s-sdcard" = {
-        wantedBy = [ "multi-user.target" ];
-        description = "Disable Thinkpad T480s SD-card";
-        script = "echo 2-3 >> /sys/bus/usb/drivers/usb/unbind";
-      };
     })
     (mkIf cfg.t480s.undervolt {
       services.throttled = {
