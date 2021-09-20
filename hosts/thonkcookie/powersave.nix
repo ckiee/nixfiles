4b42002dc74bef6e4d385c736d56d2a915bae70b
ckie <git-525ff67@ckie.dev>
2021-09-21 01:43:38 +0300

    thonkcookie: moreee power savings!!

--- hosts/thonkcookie/powersave.nix
+++ hosts/thonkcookie/powersave.nix
@@ -2,9 +2,32 @@
 
 {
   powerManagement.powertop.enable = true;
-  services.tlp.enable = true;
+
+  # Lenovo does not support this, but we turn it on anyway
+  # because we can!
+  boot.kernelParams = [ "pcie_aspm=force" ];
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Most of the defaults are okay, but we want
+      # to utilize our now-enabled ASPM
+      PCIE_ASPM_ON_BAT = "powersupersave"; # ON_AC default
+    };
+  };
   networking.networkmanager.wifi.powersave = true;
   powerManagement.cpuFreqGovernor = "powersave";
+  # iwlwifi experimental powersave
+  # https://wiki.archlinux.org/title/Power_management#Intel_wireless_cards_(iwlwifi)
+  boot.extraModprobeConfig = ''
+    options iwlwifi power_save=1
+    options iwlmvm power_scheme=3
+
+    # This is in the Arch wiki but it adds random
+    # 100ms spikes that I do not like..
+    # options iwlwifi uapsd_disable=0
+  '';
+
   # The SD-card reader is power hungry. Kill it!
   # https://wiki.archlinux.org/title/Lenovo_ThinkPad_T480s#SD_card_reader
   systemd.services."t480s-sdcard" = {
