e822890fddc23d4cc5047386fe38c82f8b43fd3e
ckie <git-525ff67@ckie.dev>
2021-09-20 21:29:12 +0300

    services/minecraft: add heapAllocation opt

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -19,6 +19,11 @@ let
 in with lib; {
   options.cookie.services.minecraft = {
     enable = mkEnableOption "Enables the Minecraft server service";
+    heapAllocation = mkOption {
+      type = types.str;
+      default = "3G";
+      description = "JVM heap allocation with specifiable size unit";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -28,7 +33,7 @@ in with lib; {
       openFirewall = true;
       package = paper;
       jvmOpts =
-        "-Xms3G -Xmx3G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
+        "-Xms${cfg.heapAllocation} -Xmx${cfg.heapAllocation} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
     };
 
     cookie.restic = let mcExec = c: "${console}/bin/mc ${escapeShellArg c}";
