bd88c4e8e92102590e1f2b1d368f059cc919a930
ckie <git-525ff67@ckie.dev>
2021-09-22 21:51:23 +0300

    collections/chat: discord pin: 0.0.15 -> 0.0.16

--- modules/collections/home/chat.nix
+++ modules/collections/home/chat.nix
@@ -9,24 +9,16 @@ in with lib; {
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      discord
       (discord-ptb.overrideAttrs (_: {
         src = builtins.fetchTarball
           "https://dl-ptb.discordapp.net/apps/linux/0.0.26/discord-ptb-0.0.26.tar.gz";
       }))
+      (discord.overrideAttrs (_: {
+        src = builtins.fetchTarball
+          "https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.tar.gz";
+      }))
       fractal
     ];
     cookie.weechat.enable = true;
-
-    # Bump discord without bumping nixpkgs
-    # nixpkgs.overlays = [
-    #   (self: super: {
-    #     # discord = super.discord.overrideAttrs (_: {
-    #     #   src = builtins.fetchTarball
-    #     #     "https://dl.discordapp.net/apps/linux/0.0.15/discord-0.0.15.tar.gz";
-    #     # });
-    #     #discord-ptb = super.;
-    #   })
-    # ];
   };
 }
