72fca699885279a4a8e0abee608d3c7113b42bea
ckie <git-525ff67@ckie.dev>
2021-10-01 19:22:40 +0300

    niv: bump to private nixpkgs

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -13,54 +13,52 @@ in with lib; {
       user = "ckie";
       dataDir = config.cookie.user.home;
       # This mapAttrs variant only changes V, not KV. Do not be fooled (:
-      declarative = {
-        overrideDevices = true;
-        overrideFolders = true;
 
-        # see /ext/metadata.toml
-        devices = let
-          trackedHosts = mapAttrs (name: host: {
-            id = host.syncthing_id;
-            inherit name;
-          }) (filterAttrs
-            (name: host: host ? syncthing_id) # host.syncthing_id = null | str
-            config.cookie.metadata.raw.hosts);
-          untrackedHosts = {
-            phone = {
-              id =
-                "5UBCVLJ-TV7IVNG-CFTXONW-Z7YUSPJ-QCQSJC4-6T2BTAB-5BNNCD4-HETZSA7";
-            };
+      overrideDevices = true;
+      overrideFolders = true;
+
+      # see /ext/metadata.toml
+      devices = let
+        trackedHosts = mapAttrs (name: host: {
+          id = host.syncthing_id;
+          inherit name;
+        }) (filterAttrs
+          (name: host: host ? syncthing_id) # host.syncthing_id = null | str
+          config.cookie.metadata.raw.hosts);
+        untrackedHosts = {
+          phone = {
+            id =
+              "5UBCVLJ-TV7IVNG-CFTXONW-Z7YUSPJ-QCQSJC4-6T2BTAB-5BNNCD4-HETZSA7";
           };
-        in untrackedHosts // trackedHosts;
+        };
+      in untrackedHosts // trackedHosts;
 
+      folders = let home = config.cookie.user.home;
+      in {
+        "nixfiles" = {
+          path = "${home}/git/nixfiles";
+          devices = [ "cookiemonster" "thonkcookie" ];
+        };
 
-        folders = let home = config.cookie.user.home;
-        in {
-          "nixfiles" = {
-            path = "${home}/git/nixfiles";
-            devices = [ "cookiemonster" "thonkcookie" ];
+        "sync" = {
+          path = "${home}/Sync";
+          devices = [ "cookiemonster" "thonkcookie" ];
+        };
+
+        "ssh" = {
+          path = "${home}/.ssh";
+          devices = [ "cookiemonster" "thonkcookie" ];
+          versioning = {
+            type = "simple";
+            params.keep =
+              "50"; # keep 50 old versions of files around. should be fine considering keys are quite small.
           };
+        };
 
-          "sync" = {
-            path = "${home}/Sync";
-            devices = [ "cookiemonster" "thonkcookie" ];
-          };
-
-          "ssh" = {
-            path = "${home}/.ssh";
-            devices = [ "cookiemonster" "thonkcookie" ];
-            versioning = {
-              type = "simple";
-              params.keep =
-                "50"; # keep 50 old versions of files around. should be fine considering keys are quite small.
-            };
-          };
-
-          "music" = {
-            id = "3ffxr-fpjwy"; # to keep compat with existing phone
-            path = "${home}/Music";
-            devices = [ "cookiemonster" "thonkcookie" "phone" ];
-          };
+        "music" = {
+          id = "3ffxr-fpjwy"; # to keep compat with existing phone
+          path = "${home}/Music";
+          devices = [ "cookiemonster" "thonkcookie" "phone" ];
         };
       };
     };
