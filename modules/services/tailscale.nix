15a2561c10af300080bfccc3a15d30f6003ad10f
ckie <git-525ff67@ckie.dev>
2021-10-03 15:05:06 +0300

    services/tailscale: init

---
+++ modules/services/tailscale.nix
@@ -0,0 +1,37 @@
+{ lib, config, pkgs, ... }:
+
+let
+  cfg = config.cookie.services.tailscale;
+  tailscale = config.services.tailscale.package;
+in with lib; {
+  options.cookie.services.tailscale = {
+    enable =
+      mkEnableOption "Enables and autoconfigures the Tailscale client daemon";
+  };
+
+  config = mkIf cfg.enable {
+    services.tailscale.enable = true;
+
+    cookie.secrets.tailscale-authkey = {
+      source = "./secrets/tailscale-authkey";
+      owner = "root";
+      group = "root";
+      permissions = "0400";
+      wantedBy = "tailscaled-autoconfig.service";
+    };
+
+    systemd.services.tailscaled-autoconfig = rec {
+      description = "Autoconfigure tailscaled";
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "tailscaled.service" "tailscale-authkey-key.service" ];
+      after = requires;
+
+      serviceConfig.Type = "oneshot";
+
+      script =
+        "${tailscale}/bin/tailscale up --login-server https://tailnet.ckie.dev --authkey $(cat ${
+          escapeShellArg config.cookie.secrets.tailscale-authkey.dest
+        })";
+    };
+  };
+}
