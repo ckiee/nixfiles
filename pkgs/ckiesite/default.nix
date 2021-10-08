0444045d8ed543c0944b347f2747ee890cdb4284
ckie <git-525ff67@ckie.dev>
2021-10-09 02:30:51 +0300

    pkgs/ckiesite: use niv, bump

--- pkgs/ckiesite/default.nix
+++ pkgs/ckiesite/default.nix
@@ -1,15 +1,12 @@
 { lib, stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+let sources = import ../../nix/sources.nix;
+    src = sources.ckiesite;
+in stdenv.mkDerivation rec {
   pname = "ckiesite";
   version = "git";
 
-  src = fetchFromGitHub {
-    owner = "ckiee";
-    repo = "ckiesite";
-    rev = "bb695a2b197d81a6d9045e114181ba535e00e2fb";
-    sha256 = "sha256-S/SV5d/M5PyJ/zsWT5BjlypFF1aulPqnG6+cyqj7F2U=";
-  };
+  inherit src;
 
   phases = "installPhase";
   installPhase = ''
