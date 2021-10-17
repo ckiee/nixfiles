a688089465394bbe5ce9c92605f418efaf3527d8
ckie <git-525ff67@ckie.dev>
2021-10-17 14:54:21 +0300

    st: allowwindowops=1

--- ext/st.h
+++ ext/st.h
@@ -45,7 +45,7 @@ int allowaltscreen = 1;
 
 /* allow certain non-interactive (insecure) window operations such as:
    setting the clipboard text */
-int allowwindowops = 0;
+int allowwindowops = 1;
 
 /*
  * draw latency range in ms - from new content/keypress/etc until drawing.
