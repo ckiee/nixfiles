f49537a0e9a1e2e11b903ad682c33c911f2d1608
ckie <git-525ff67@ckie.dev>
2021-11-20 22:40:13 +0200

    hosts/installer: fixup

--- hosts/installer/install.nix
+++ hosts/installer/install.nix
@@ -8,50 +8,57 @@ let
         modules = [ ../thonkcookie ];
       }).config.system.build.toplevel;
     in writeShellScriptBin "installer" ''
-      [ $USER != root ] && sudo $(realpath "$${""}{0}") "$${""}{@}"; exit $?
-      INSTALL_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%^&*()' | fold -w 8 | head -n 1)
-
+      set -eu
+      INSTALL_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
       [ -d /sys/firmware/efi/efivars/ ] || (echo "Aborting, not running in EFI!" && exit 1)
 
       # let's just do it in the background async
       nix-store --verify-path ${system} &
 
-      sudo sfdisk --wipe always /dev/vda <<-END
+      sudo sfdisk --wipe always $1 <<-END
       label: gpt
       boot : start=2048, size=204800, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name=boot-$INSTALL_ID
-      zfs  : start=206848, type=516E7CBA-6ECF-11D6-8FF8-00022D09712B, name=zfs-$INSTALL-ID
+      zfs  : start=206848, type=516E7CBA-6ECF-11D6-8FF8-00022D09712B, name=zfs-$INSTALL_ID
       END
 
-      mkfs.vfat /dev/disk/by-partlabel/boot-$INSTALL_ID
+      for part in zfs boot; do
+        echo Waiting for label $part-$INSTALL_ID
+        while true; do [ -L /dev/disk/by-partlabel/$part-$INSTALL_ID ] && break; done
+      done
 
-      zpool create
-        -o ashift=12 \
-        -O mountpoint=none \
-        -O atime=off \
-        -O acltype=posixacl \
-        -O xattr=sa \
-        -O compression=lz4 \
-        -O keyformat=passphrase \
-        -O encryption=on \
-        -O refreservation=1G \
-        zroot /dev/disk/by-partlabel/zfs-$INSTALL_ID
+      sudo mkfs.vfat /dev/disk/by-partlabel/boot-$INSTALL_ID
 
-      zfs create -p -o mountpoint=legacy zroot/local/nix
-      zfs create -p -o mountpoint=legacy zroot/local/root
-      zfs create -p -o mountpoint=legacy zroot/safe/persist
 
-      zfs snapshot rpool/local/root@blank
+      for idx in $(seq 3); do
+        sudo zpool create \
+          -o ashift=12 \
+          -O mountpoint=none \
+          -O atime=off \
+          -O acltype=posixacl \
+          -O xattr=sa \
+          -O compression=lz4 \
+          -O keyformat=passphrase \
+          -O encryption=on \
+          -O refreservation=1G \
+          zroot /dev/disk/by-partlabel/zfs-$INSTALL_ID || true
+      done
 
-      mkdir /mnt
-      mount -t zfs zroot/local/root /mnt
-      mkdir /mnt/{persist,nix}
-      mount -t zfs zroot/local/nix /mnt/nix
-      mount -t zfs zroot/safe/persist /mnt/persist
+      sudo zfs create -p -o mountpoint=legacy zroot/local/nix
+      sudo zfs create -p -o mountpoint=legacy zroot/local/root
+      sudo zfs create -p -o mountpoint=legacy zroot/safe/persist
 
-      nixos-generate-config --show-hardware-config --root /mnt >/mnt/persist/hardware.nix
+      sudo zfs snapshot zroot/local/root@blank
 
-      fg
-      ${config.system.build.nixos-install}/bin/nixos-install \
+      sudo mount -t zfs zroot/local/root /mnt
+      sudo mkdir /mnt/{persist,nix,boot}
+      sudo mount /dev/disk/by-partlabel/boot-$INSTALL_ID /mnt/boot
+      sudo mount -t zfs zroot/local/nix /mnt/nix
+      sudo mount -t zfs zroot/safe/persist /mnt/persist
+
+      #nixos-generate-config --show-hardware-config --root /mnt >/mnt/persist/hardware.nix
+
+      fg || true
+      sudo ${config.system.build.nixos-install}/bin/nixos-install \
         --system ${system} \
         --no-root-passwd \
         --cores 0
