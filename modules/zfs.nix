0fb6fdbb4150f4d28225667e816d2d1898f1f04f
ckie <git-525ff67@ckie.dev>
2021-11-24 15:01:04 +0200

    zfs: comment

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -6,6 +6,7 @@ in with lib; {
   options.cookie.zfs = { enable = mkEnableOption "Enables ZFS management"; };
 
   config = mkIf cfg.enable {
+    # This module works alongside the autoinstaller
     boot = {
       initrd.supportedFilesystems = [ "zfs" ];
       zfs = {
@@ -13,6 +14,7 @@ in with lib; {
         devNodes = "/dev/disk/by-partlabel";
       };
     };
+
     networking.hostId = pkgs.lib.concatStringsSep "" (pkgs.lib.take 8
       (pkgs.lib.stringToCharacters
         (builtins.hashString "sha256" config.networking.hostName)));
