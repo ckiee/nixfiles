2d3e8113e56d8d713ea75da4f26cadc4bc53b5f9
ckie <git-525ff67@ckie.dev>
2021-11-24 12:08:44 +0200

    desktop: remove invalid states from monitor opts

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -5,15 +5,21 @@ in with lib; {
   options.cookie.desktop = {
     enable = mkEnableOption "Enables the Cookie's desktop";
     # These are used by home/i3.nix, home/polybar.nix
-    primaryMonitor = mkOption {
-      type = types.str;
-      example = "eDP-1";
-      description = "primary output";
-    };
-    secondaryMonitor = mkOption {
-      type = types.nullOr types.str;
-      example = "eDP-1";
-      description = "secondary output";
+    monitors = mkOption {
+      type = types.nullOr (types.submodule {
+        primary = mkOption {
+          type = types.str;
+          example = "eDP-1";
+          description = "primary output";
+        };
+        secondary = mkOption {
+          type = types.nullOr types.str;
+          example = "eDP-1";
+          description = "secondary output";
+          default = null;
+        };
+      });
+      description = "monitor configuration";
       default = null;
     };
     laptop = mkEnableOption "Enables laptop-specific customizations";
