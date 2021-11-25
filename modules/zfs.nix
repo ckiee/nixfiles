ab57dc744e8be0afe5601b96bf85edffdefb773d
ckie <git-525ff67@ckie.dev>
2021-11-25 18:56:37 +0200

    zfs: add manageAZroot option to scrub

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -3,20 +3,40 @@
 let cfg = config.cookie.zfs;
 
 in with lib; {
-  options.cookie.zfs = { enable = mkEnableOption "Enables ZFS management"; };
-
-  config = mkIf cfg.enable {
-    # This module works alongside the autoinstaller
-    boot = {
-      initrd.supportedFilesystems = [ "zfs" ];
-      zfs = {
-        forceImportRoot = false;
-        devNodes = "/dev/disk/by-partlabel";
-      };
+  options.cookie.zfs = {
+    enable = mkEnableOption "Enables ZFS management";
+    manageZroot = mkOption {
+      type = types.bool;
+      default = config.fileSystems."/".fsType == "zfs";
+      description = "Whether this machine should manage zroot";
     };
-
-    networking.hostId = pkgs.lib.concatStringsSep "" (pkgs.lib.take 8
-      (pkgs.lib.stringToCharacters
-        (builtins.hashString "sha256" config.networking.hostName)));
   };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      # This module works alongside the autoinstaller
+      boot = {
+        initrd.supportedFilesystems = [ "zfs" ];
+        zfs = {
+          forceImportRoot = false;
+          devNodes = "/dev/disk/by-partlabel";
+        };
+      };
+
+      networking.hostId = pkgs.lib.concatStringsSep "" (pkgs.lib.take 8
+        (pkgs.lib.stringToCharacters
+          (builtins.hashString "sha256" config.networking.hostName)));
+    })
+
+    (mkIf (cfg.manageZroot && cfg.enable) {
+      services.zfs = {
+        autoScrub = {
+          enable = true;
+          interval = "monthly";
+          pools = [ "zroot" ];
+        };
+        # TODO let zfs send mail
+      };
+    })
+  ];
 }
