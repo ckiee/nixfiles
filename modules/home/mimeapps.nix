5be178b07a294762e15e33180c7a63ebbc0a9b51
ckie <git-525ff67@ckie.dev>
2021-12-13 16:44:28 +0200

    {desktop,home/mimeapps}: finalize & enable

--- modules/home/mimeapps.nix
+++ modules/home/mimeapps.nix
@@ -10,10 +10,36 @@ in with lib; {
   config = mkIf cfg.enable {
     xdg.mimeApps = {
       enable = true;
-      defaultApplications = {
-        "application/pdf" = [ "firefox.desktop" ];
-        "text/plain" = [ "emacsclientexs.desktop" ];
-        "text/x-java" = [ "emacsclientexs.desktop" ];
+      defaultApplications = let
+        emacs = "emacsclientexs.desktop";
+        firefox = "firefox.desktop";
+        mpv = "mpv.desktop";
+      in {
+        "application/pdf" = [ firefox ];
+        "text/plain" = [ emacs ];
+        "text/x-java" = [ emacs ];
+        "text/html" = [ emacs ];
+        "application/xhtml+xml" = [ emacs ];
+        "text/yaml" = [ emacs ];
+        "text/x-c++src" = [ emacs ];
+        "x-scheme-handler/magnet" =
+          [ "userapp-transmission-gtk-29NL20.desktop" ];
+        "text/x-csrc" = [ emacs ];
+        "audio/x-vorbis+ogg" = [ "audacity.desktop" "vlc.desktop" mpv ];
+        "audio/mpeg" = [ mpv "vlc.desktop" "audacity.desktop" ];
+        "text/x-sh" = [ emacs ];
+        "application/x-java-archive" = [ "org.gnome.FileRoller.desktop" ];
+        "text/x-lisp" = [ emacs ];
+        "image/png" = [ "feh.desktop" ];
+        "video/mp4" = [ "vlc.desktop" mpv ];
+        "application/xml" = [ emacs ];
+        "video/quicktime" = [ mpv ];
+        "audio/x-wav" = [ mpv ];
+        "video/x-matroska" = [ mpv ];
+        "application/octet-stream" = [ emacs ];
+        "audio/mp4" = [ mpv ];
+        "binary/octet-stream" = [ emacs ];
+        "audio/flac" = [ mpv ];
       };
     };
   };
