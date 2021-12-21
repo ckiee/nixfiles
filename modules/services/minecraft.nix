6c2c6c059ed792741fd9ff221400bc9667d03f27
ckie <git-525ff67@ckie.dev>
2021-12-21 14:24:17 +0200

    services/minecraft: oops

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -10,7 +10,7 @@ let
       in "https://papermc.io/api/v2/projects/paper/versions/${mc}/builds/${
         toString build
       }/downloads/paper-${mc}-${toString build}.jar";
-      sha256 = "sha256-7dY1dfziGEPlKmh/eRfqXP55ouQHeXiR6PnPFoAgZwQ=";
+      sha256 = "sha256-Qpk/IAg5ExNJ5JbnTZRIZ3+7lRfG/oyafKz755ilf9o=";
     };
   });
   console = pkgs.writeShellScriptBin "mc" ''
