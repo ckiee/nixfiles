e1c2f48fa89070e94f71086edc884e3def134543
ckie <git-525ff67@ckie.dev>
2021-12-24 16:24:05 +0200

    secrets: significantly faster encryption!

---
+++ hosts/_metadata/default.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, lib, nodes, ... }:
+with lib;
+
+let
+  host = "cookiemonster";
+  userPubkey = lib.fileContents ../../ext/id_ed25519.pub;
+  filenameFromPath = path: last (splitString "/" path);
+in {
+  imports = [ ../../modules/metadata.nix ];
+  options.system = {
+    # Mock for morph
+    nixos.release = mkOption {
+      readOnly = true;
+      type = types.str;
+      default = "00.00";
+    };
+    build.toplevel = mkOption {
+      readOnly = true;
+      default = pkgs.writeScript "cookie-rager-encrypt" ''
+        set -e
+        cd $(${pkgs.git}/bin/git rev-parse --show-toplevel)
+        ${concatStringsSep "\n" (mapAttrsToList (host: hostConfig:
+          let
+            cfg = hostConfig.config.cookie.secrets;
+            machinePubkey = config.cookie.metadata.raw.hosts.${host}.ssh_pubkey;
+          in ''
+            mkdir -p encrypted/'${host}' || true
+            ${concatStringsSep "\n" (mapAttrsToList (_: secret:
+              let
+                secretFn = filenameFromPath secret.source;
+              in ''
+                if [ "$(cat encrypted/'${host}'/'${secretFn}'.HASH)" != "$(sha512sum '${secret.source}')" ]; then
+                  echo "[${secretFn}:${host}] sha512 changed, re-encrypting"
+                  ${pkgs.rage}/bin/rage -a -r '${machinePubkey}' -r '${userPubkey}' -o 'encrypted/${host}/${secretFn}' '${secret.source}'
+                fi
+                sha512sum '${secret.source}' > encrypted/'${host}'/'${secretFn}'.HASH
+              '') cfg)}
+          '') ((filterAttrs (host: _: host != "_metadata")) nodes))}
+      '';
+    };
+  };
+}
--- shell.nix
+++ shell.nix
@@ -3,90 +3,22 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-  inherit (pkgs) lib;
-  bpkg = pkgs.writeScriptBin "bpkg" ''
-    COOKIE_TOPLEVEL=$(${pkgs.git}/bin/git rev-parse --show-toplevel)
-    nix-build "$COOKIE_TOPLEVEL/pkgs" -A "$@"
-  '';
+
   # throw is a keyword
   throwDeriv = pkgs.writeScriptBin "throw" ''
-    mo deploy morph.nix switch --passwd --on $@
+    "$(morph build morph.nix --on=_metadata 2>/dev/null)"/_metadata
+    morph deploy morph.nix switch --passwd --on $@
   '';
-  rager = pkgs.writeScriptBin "rager" ''
-        export PATH=$PATH:${lib.makeBinPath (with pkgs; [ rage mo ])}
-        set -e
-        export COOKIE_RAGER_BUILD=1
-        export COOKIE_TOPLEVEL="$(${pkgs.git}/bin/git rev-parse --show-toplevel)"
-        cd "$COOKIE_TOPLEVEL"
-        function show_help {
-          >&2 echo "Usage: rager {encrypt,decrypt,wrap,dwim}"
-        }
-        case "$1" in
-          encrypt)
-            if [ ! -d secrets ]
-            then
-              >&2 echo "no unencrypted secrets to encrypt"
-              exit 1
-            fi
 
-            for machine in "$(mo build morph.nix)/"*
-              do "$machine"/sw/cookie-rager-encrypt
-            done
-            rm -rf secrets
-            ;;
-          decrypt)
-            tmp=$(mktemp -d)
-            find "$(pwd)/encrypted" -type f -exec cp -nst "$tmp" {} + || true
-            if [ -d secrets ]
-            then
-              >&2 echo "not continuing with existing secrets directory"
-              rm -rf "$tmp"
-              exit 1
-            fi
-            mkdir secrets
-            for f in "$tmp"/*
-                do out=secrets/"$(basename "$f")"
-                rage -di ~/.ssh/id_rsa -i ~/.ssh/id_ed25519 -o "$out" "$f"
-                chmod 600 "$out"
-            done
-            rm -rf "$tmp"
-            ;;
-          dwim)
-            $0 encrypt
-            $0 decrypt
-            ;;
-          wrap)
-            self="$0"
-            do_encrypt=0
-            if [ ! -d secrets ]; then
-              $self decrypt
-              do_encrypt=1
-            fi
-            shift
-            $@ || true
-            if [ "$do_encrypt" != "0" ]; then
-              $self encrypt
-            fi
-            ;;
-          *)
-            show_help
-            exit 1
-            ;;
-        esac
-      '';
-  mo = pkgs.writeScriptBin "mo" ''
-    COOKIE_TOPLEVEL=$(${pkgs.git}/bin/git rev-parse --show-toplevel) ${pkgs.morph}/bin/morph $@
-  '';
+  morph = import sources.morph { inherit pkgs; };
 in pkgs.mkShell {
   NIX_PATH = "nixpkgs=${pkgs.path}";
 
   buildInputs = with pkgs; [
     niv
-    mo
+    morph
     nix-prefetch-scripts
     nix-prefetch-github
-    bpkg
-    rager
     throwDeriv
     nixos-generators
   ];
