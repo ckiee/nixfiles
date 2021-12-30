2d61bbc200fac86b977eea06beb034d93849ed2d
ckie <git-525ff67@ckie.dev>
2021-12-31 00:27:32 +0200

    morph: rework evaluator with upstream nixpkgs changes

--- morph.nix
+++ morph.nix
@@ -1,3 +1,5 @@
+with builtins;
+
 let
   sources = import ./nix/sources.nix;
   networkPkgs = import sources.nixpkgs { allowUnfree = true; };
@@ -10,23 +12,24 @@ in {
       # This is a wrapper around the NixOS evalConfig to add an edge-case for our _metadata host
       evalConfigArgs@{ extraArgs ? { }, specialArgs ? { }, modules, check ? true
       , prefix ? [ ] }:
-      let originalFn = import (networkPkgs.path + "/nixos/lib/eval-config.nix");
-      in originalFn (if extraArgs.name == "_metadata" && check then
-        evalConfigArgs // {
-          baseModules = [
-            ({ ... }: {
-              _module.args.pkgs = networkPkgs;
-              _module.args.nodes = extraArgs.nodes;
-              _module.check = false;
-            })
-          ];
-          # extraArgs = evalConfigArgs.extraArgs // { pkgs = networkPkgs; };
-        }
-      else
-        evalConfigArgs // {
-          modules = modules ++ [ ({ ... }: { deployment.tags = [ "real" ]; }) ];
-        });
-
+      let
+        originalFn = import (networkPkgs.path + "/nixos/lib/eval-config.nix");
+        isMetadata = extraArgs.name == "_metadata" && check;
+      in originalFn
+      ((removeAttrs evalConfigArgs [ "extraArgs" "specialArgs" "check" ]) // {
+        modules = modules ++ [
+          ({ ... }: {
+            _module = {
+              check = if isMetadata then false else check;
+              args = extraArgs;
+            };
+            deployment.tags = networkPkgs.lib.optional (!isMetadata) "real";
+          })
+        ];
+      } // (if isMetadata then {
+        baseModules = [ ({ ... }: { _module.args.pkgs = networkPkgs; }) ];
+      } else
+        { }));
   };
 
   # Tailscale hosts
