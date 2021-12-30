64159ea0dd6df63797ff09f04df4f13a7e536829
ckie <git-525ff67@ckie.dev>
2021-12-30 19:00:36 +0200

    hosts/pansear: let's just pass through a windows VM

---
+++ hosts/pansear/windows-passthrough.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+
+{
+  users.users.win = {
+    isNormalUser = true;
+    # empty password:
+    hashedPassword =
+      "$6$HpJwJOJx9KR$4tR8WgBq4m8GdSxrK6920WxnklprO4hQfAJa2xNKdfoG9bbPBG3rEEx292Dat/RNe1Bjnu..O860JJhpN8B2R.";
+  };
+
+  services.xserver = {
+    enable = true;
+    displayManager.session = [({
+      manage = "desktop";
+      name = "windows";
+      start =
+        "${pkgs.freerdp}/bin/xfreerdp /u:Mik /p:mik /v:192.168.122.2 /cert:ignore +auto-reconnect /auto-reconnect-max-retries:0 +clipboard +home-drive /f /dynamic-resolution -toggle-fullscreen";
+    })];
+    displayManager = {
+      hiddenUsers = [ "ckie" ];
+      autoLogin = {
+        enable = true;
+        user = "win";
+      };
+    };
+  };
+}
