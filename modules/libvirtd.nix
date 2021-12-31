c8683f9f050214164a70b6e9cf208fbf348a782a
ckie <git-525ff67@ckie.dev>
2021-12-31 21:06:10 +0200

    libvirtd: add a fake dnsmasq

--- modules/libvirtd.nix
+++ modules/libvirtd.nix
@@ -11,7 +11,16 @@ in with lib; {
     users.users.ckie.extraGroups = [ "libvirtd" ];
 
     virtualisation = {
-      libvirtd = { enable = true; };
+      libvirtd = {
+        enable = true;
+        package = pkgs.libvirt.override {
+          # I hate dnsmasq, it ends up in my dreams and breaks my builds
+          # in weird ways and takes over my ports and attacks my fleet and is just rude!
+          # Maybe it's not it's fault, but it has caused so many problems due to it's environment.
+          # I wish I could help it but the best I can do is to hide it, away from it's abusers.
+          dnsmasq = pkgs.writeShellScriptBin "dnsmasq" "sleep infinity";
+        };
+      };
       spiceUSBRedirection.enable = true;
     };
     boot.kernel.sysctl."net.ipv4.ip_forward" = true;
