033b759524673dc5f068ea2eb7c13e9c4ca7991a
ckie <git-525ff67@ckie.dev>
2022-01-01 00:20:34 +0200

    remote-builder: suck much, much less

--- modules/remote-builder.nix
+++ modules/remote-builder.nix
@@ -1,11 +1,13 @@
-{ lib, config, pkgs, ... }:
+{ nodes, lib, config, pkgs, ... }:
+
+with builtins;
+with lib;
 
 let
   cfg = config.cookie.remote-builder;
-  nix-store = ''
-    NIX_SSHOPTS="-oStrictHostKeyChecking=no -oIdentityFile=/home/ckie/.ssh/id_ed25519" ${config.nix.package}/bin/nix-store'';
+  nix-store = "${config.nix.package}/bin/nix-store --serve --write";
   wrapper = pkgs.writeScript "cookie-remote-build" "sudo ${nix-store} $@";
-in with lib; {
+in {
   options.cookie.remote-builder = {
     role = mkOption {
       type = types.nullOr (types.enum [ "builder" "user" ]);
@@ -27,28 +29,45 @@ in with lib; {
       users.users.remote-builder = {
         isSystemUser = true;
         useDefaultShell = true;
-        openssh.authorizedKeys.keys = [
-          # see https://github.com/ckiee/nix/blob/d40828224ae0d47acc70e5431dd2f42775085ebe/src/libstore/legacy-ssh-store.cc#L75-L76
+        openssh.authorizedKeys.keys = map (key:
           ''
-            no-port-forwarding,no-X11-forwarding,no-pty,restrict,command="sudo ${wrapper} --serve --write" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3uTwzSSMAPg84fwbNp2cq9+BdLFeA1VzDGth4zCAbz https://ckie.dev''
-        ];
+            no-port-forwarding,no-X11-forwarding,no-pty,restrict,command="sudo ${wrapper}" ${key}'')
+          [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOXurLSbdSEr5mJH0dmLjqbaSAl2amq/Lh5tuNI90Q3 buildfarm"
+          ];
         group = "remote-builder";
       };
       users.groups.remote-builder = { };
     })
 
     (mkIf (cfg.role == "user") {
-      nix.buildMachines = [{
-        hostName = "remote-builder@pansear";
-        system = "x86_64-linux";
-        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        mandatoryFeatures = [ ];
-      }];
       nix.distributedBuilds = true;
       nix.extraOptions = ''
         # useful when the builder has a faster internet connection than yours
         builders-use-substitutes = true
       '';
+      nix.buildMachines = [{
+        hostName = "remote-builder@pansear";
+        system = "x86_64-linux";
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        mandatoryFeatures = [ ];
+        maxJobs = 16;
+      }];
+
+      # Nix needs to be given SSH details! Naturally, we encrypt the key
+
+      cookie.secrets.ssh-buildfarm-key = {
+        source = "./secrets/ssh_buildfarm_key";
+        permissions = "0400";
+        wantedBy = "nix-daemon.service";
+      };
+      systemd.services.nix-daemon.environment.NIX_SSHOPTS = let
+        knownHosts = pkgs.writeText "known-hosts" ''
+          ${concatStringsSep "\n" (mapAttrsToList (name: node:
+            "${name} ${node.config.cookie.machine-info.sshPubkey}") nodes)}
+          # external hosts here:
+        '';
+      in "-oIdentityFile=${config.cookie.secrets.ssh-buildfarm-key.dest} -oUserKnownHostsFile=${knownHosts}";
     })
   ];
 }
