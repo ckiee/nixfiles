2c8318571b40ba4c8ff1c3aff32f9af2ff0fdc2c
ckie <git-525ff67@ckie.dev>
2022-01-01 06:46:46 +0200

    doom-emacs: bump, try out vertico

--- modules/doom-emacs/config/init.el
+++ modules/doom-emacs/config/init.el
@@ -23,7 +23,8 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons +prescient)               ; a search engine for love and life
+       ;;(ivy +icons +prescient)               ; a search engine for love and life
+       (vertico +icons)
 
        :ui
        ;;deft              ; notational velocity for Emacs
