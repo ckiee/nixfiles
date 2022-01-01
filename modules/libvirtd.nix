8413c776a4699230de5a83b94207b66be3f7a477
ckie <git-525ff67@ckie.dev>
2022-01-01 02:05:43 +0200

    libvirtd: improve dnsmasq wrapper

--- modules/libvirtd.nix
+++ modules/libvirtd.nix
@@ -18,7 +18,12 @@ in with lib; {
           # in weird ways and takes over my ports and attacks my fleet and is just rude!
           # Maybe it's not it's fault, but it has caused so many problems due to it's environment.
           # I wish I could help it but the best I can do is to hide it, away from it's abusers.
-          dnsmasq = pkgs.writeShellScriptBin "dnsmasq" "sleep infinity";
+          #
+          # Anyway, we only sleep forever if it's passing the first arg it does when it's trying to run it as a daemon
+          dnsmasq = pkgs.writeShellScriptBin "dnsmasq" ''
+           [ "$1" == "--conf-file=/var/lib/libvirt/dnsmasq/default.conf" ] && sleep infinity
+           ${pkgs.dnsmasq}/bin/dnsmasq $@
+          '';
         };
       };
       spiceUSBRedirection.enable = true;
