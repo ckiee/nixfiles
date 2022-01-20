fab8976aeadaf5bb91379baa002a1e3e0b959bfc
ckie <git-525ff67@ckie.dev>
2022-01-20 17:52:21 +0200

    caches: add cache.tailnet.ckie.dev

--- modules/caches.nix
+++ modules/caches.nix
@@ -7,9 +7,13 @@ in with lib; {
   };
 
   config.nix = mkIf cfg.enable {
-    binaryCaches =
-      [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
+    binaryCaches = [
+      "https://cache.tailnet.ckie.dev"
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
     binaryCachePublicKeys = [
+      "cache.tailnet.ckie.dev:Ng8W2u5lGtakekcMxEy7vaw99IwgDaK8ensVZQfZgUQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
