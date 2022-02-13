c0809c77a5a1b5f8eb395c2e8a260a8d1a113bdb
ckie <git-525ff67@ckie.dev>
2022-02-13 10:55:33 +0200

    default: enable boot.cleanTmpDir

--- default.nix
+++ default.nix
@@ -17,6 +17,7 @@ with builtins;
 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.supportedFilesystems = [ "ntfs" "btrfs" ];
+  boot.cleanTmpDir = true;
 
   networking.networkmanager.enable = true;
 
