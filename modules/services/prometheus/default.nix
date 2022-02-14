3e1fb7216c435010ac3a8334885e9d41df17324b
ckie <git-525ff67@ckie.dev>
2022-02-14 09:04:05 +0200

    services/prometheus: alerting but theres no alerter yet

--- modules/services/prometheus/default.nix
+++ modules/services/prometheus/default.nix
@@ -19,7 +19,7 @@ in with lib; {
     enable = mkEnableOption "Enables the Prometheus monitoring service";
     nginx-vhosts = mkOption rec {
       type = types.listOf types.str;
-      default = [];
+      default = [ ];
       description = "List of nginx virtual host names";
     };
   };
@@ -28,17 +28,60 @@ in with lib; {
     # https://grahamc.com/blog/nixos-system-version-prometheus
     system.activationScripts = { inherit node-exporter-system-version; };
 
+    cookie.secrets.prom-alert-webhook = {
+      source = "./secrets/prom-alert-webhook.nix";
+      runtime = false;
+    };
+
     services.prometheus = {
       enable = true;
       globalConfig.scrape_interval = "5s";
       rules = [ (builtins.readFile ./node_rules.yaml) ];
+      alertmanager = {
+        enable = true;
+        configuration = {
+          global.slack_api_url = import ../../../secrets/prom-alert-webhook.nix;
+          route = {
+            group_by = [ "alertname" "cluster" "service" ];
+            group_wait = "30s";
+            group_interval = "30s";
+            repeat_interval = "30s";
+            receiver = "discord";
+          };
+
+          inhibit_rules = [{
+            source_matchers = [ ''severity="critical"'' ];
+            target_matchers = [ ''severity="warning"'' ];
+            # Apply inhibition if the alertname is the same.
+            # CAUTION:
+            #   If all label names listed in `equal` are missing
+            #   from both the source and target alerts,
+            #   the inhibition rule will apply!
+            equal = [ "alertname" "cluster" "service" ];
+          }];
+
+          # receivers = [{
+          #   name = "discord";
+          #   webhook_configs = [{
+          #     url = "http://  "
+          #     send_resolved = true;
+          #   }];
+          # }];
+        };
+      };
+      alertmanagers = [{
+        scheme = "http";
+        path_prefix = "/";
+        static_configs = [{ targets = [ "127.0.0.1:9093" ]; }];
+      }];
+
       scrapeConfigs = let
         listenMap = host: ports:
           (imap0 (i: v: ("${host}:${toString v}")) ports);
       in [
         {
           job_name = "nginx";
-          static_configs = [{ targets = listenMap "127.0.0.1" [ 9113 9117 ]; }];
+          static_configs = [{ targets = listenMap "127.0.0.1" [ 9117 ]; }];
         }
         {
           job_name = config.networking.hostName;
