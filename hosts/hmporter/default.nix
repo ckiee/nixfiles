bcda6ee939e96d1b50f65bade1a5fa2fd34630d2
ckie <git-525ff67@ckie.dev>
2022-06-10 03:23:29 +0300

    hosts/hmporter: fix hostname

--- hosts/hmporter/default.nix
+++ hosts/hmporter/default.nix
@@ -4,7 +4,7 @@
   home-manager.users.ckie = { ... }: { cookie = { st.enable = true; }; };
 
   #### Stub ####
-  networking.hostName = "hmport";
+  networking.hostName = "hmporter";
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
     fsType = "ext4";
