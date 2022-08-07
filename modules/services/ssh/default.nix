99594b26b77984418c5edf3053c679d0ec55392c
ckie <git-525ff67@ckie.dev>
2022-08-07 12:38:36 +0300

    services/ssh: add unused useAlternatePort

--- modules/services/ssh/default.nix
+++ modules/services/ssh/default.nix
@@ -5,6 +5,9 @@ let cfg = config.cookie.services.ssh;
 in with lib; {
   options.cookie.services.ssh = {
     enable = mkEnableOption "Enables the OpenSSH daemon and Mosh";
+    # unused, but maybe it should be? idk, this config is public after all.
+    useAlternatePort =
+      mkEnableOption "Exposes the SSH server on port 2222 instead";
   };
 
   config = mkIf cfg.enable {
@@ -14,7 +17,23 @@ in with lib; {
       permitRootLogin = mkForce
         "no"; # for the "install" host this is enabled, so we force it away
       passwordAuthentication = false;
+      ports = mkIf cfg.useAlternatePort (mkForce [ 2222 ]);
+      # Listen on the usual port on tailscale
+      listenAddresses = mkIf cfg.useAlternatePort [
+        {
+          addr = config.cookie.state.tailscaleIp;
+          port = 22;
+        }
+        {
+          addr = "0.0.0.0";
+          port = 2222;
+        }
+      ];
     };
+
+    networking.firewall.interfaces.tailscale0.allowedTCPPorts =
+      mkIf cfg.useAlternatePort [ 22 2222 ];
+
     services.fail2ban = mkIf config.networking.firewall.enable {
       enable = true;
       maxretry = 1;
