3152140f2a26a4de652230024c5416a71b9ca7ad
ckie <git-525ff67@ckie.dev>
2023-01-26 01:15:20 +0200

    home/polybar: i3-gaps has been merged into i3

--- modules/home/polybar/default.nix
+++ modules/home/polybar/default.nix
@@ -89,7 +89,7 @@ in {
     services.polybar = {
       enable = true;
       package = (pkgs.polybar.override {
-        i3GapsSupport = true;
+        i3Support = true;
         pulseSupport = true;
         mpdSupport = true;
       }).overrideAttrs (old: {
