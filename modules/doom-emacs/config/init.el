b4adb102d87e03b92546384aa31ddb744001ca3b
ckie <git-525ff67@ckie.dev>
2023-02-16 19:33:49 +0200

    doom-emacs: ein! jupyter (:

--- modules/doom-emacs/config/init.el
+++ modules/doom-emacs/config/init.el
@@ -89,7 +89,7 @@
        direnv
        ;;docker
        editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
