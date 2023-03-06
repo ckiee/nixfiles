85b50b183eeb3616d24580a53435f5355e98a147
ckie <git-525ff67@ckie.dev>
2023-03-06 12:59:49 +0200

    binary-caches: disable it, cachix is 5xx'ing

--- modules/binary-caches.nix
+++ modules/binary-caches.nix
@@ -9,7 +9,7 @@ in with lib; {
   config.nix.settings = mkIf cfg.enable {
     substituters = [
       "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
+      # "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
