db23607a1c66ec74d9d4dbd982994710058b0691
ckie <git-525ff67@ckie.dev>
2023-04-08 06:19:43 +0300

    Revert "libvirtd: remove dnsmasq disable hack"
    This reverts commit 58d6dabee49d04dd1d621ce33f4090762ebdb9cc.

--- modules/libvirtd.nix
+++ modules/libvirtd.nix
@@ -13,6 +13,21 @@ in with lib; {
     virtualisation = {
       libvirtd = {
         enable = true;
+        package = pkgs.libvirt.override {
+          # I hate dnsmasq, it ends up in my dreams and breaks my builds
+          # in weird ways and takes over my ports and attacks my fleet and is just rude!
+          # Maybe it's not it's fault, but it has caused so many problems due to it's environment.
+          # I wish I could help it but the best I can do is to hide it, away from it's abusers.
+          #
+          # Anyway, we only sleep forever if it's passing the first arg it does when it's trying to run it as a daemon
+          dnsmasq = pkgs.writeShellScriptBin "dnsmasq" ''
+            if echo "$@" | ${pkgs.gnugrep}/bin/grep "leasefile"; then
+              true
+            else
+              exec ${pkgs.dnsmasq}/bin/dnsmasq $@
+            fi
+          '';
+        };
       };
       spiceUSBRedirection.enable = true;
     };
