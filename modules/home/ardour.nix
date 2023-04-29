e2666512eca6fb35a82cc3a6ad64676106ae656f
ckie <git-525ff67@ckie.dev>
2023-04-29 19:52:14 +0300

    home/ardour: ~ update, add surge

--- modules/home/ardour.nix
+++ modules/home/ardour.nix
@@ -8,17 +8,22 @@ in with lib; {
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ ardour ];
+    home.packages = with pkgs; [
+      ardour
+      # TODO: split out into another module
+      surge
+      surge-XT
+    ];
     home.activation.ardourConfig = let rg = getBin pkgs.ripgrep;
     in hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ -e ~/.config/ardour6/config ]; then
+      if [ -e ~/.config/ardour7/config ]; then
         $VERBOSE_ECHO "Setting impure ardour config values"
         $DRY_RUN_CMD mkdir $VERBOSE_ARG ~/Sync/.ardour || true
-        $DRY_RUN_CMD mv $VERBOSE_ARG ~/.config/ardour6/config{,.work} &&
+        $DRY_RUN_CMD mv $VERBOSE_ARG ~/.config/ardour7/config{,.work} &&
         $DRY_RUN_CMD ${rg}/bin/rg \
-          '(<Option name="default-session-parent-dir" value=")[^"]+("/>)' ~/.config/ardour6/config.work \
-          --passthru -r '$1'"$HOME/Sync/.ardour"'$2' > ~/.config/ardour6/config &&
-        $DRY_RUN_CMD rm $VERBOSE_ARG ~/.config/ardour6/config.work
+          '(<Option name="default-session-parent-dir" value=")[^"]+("/>)' ~/.config/ardour7/config.work \
+          --passthru -r '$1'"$HOME/Sync/.ardour"'$2' > ~/.config/ardour7/config &&
+        $DRY_RUN_CMD rm $VERBOSE_ARG ~/.config/ardour7/config.work
       fi
     '';
   };
