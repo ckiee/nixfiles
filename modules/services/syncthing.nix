2c4a462332fd814fbfa9360003981843771925b4
ckie <git-525ff67@ckie.dev>
2023-06-29 18:58:15 +0300

    services/syncthing: it didn't like that

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -22,69 +22,65 @@ in with lib; {
       overrideDevices = true;
       overrideFolders = true;
 
-      settings = {
-        devices = let
-          trackedHosts = mapAttrs (host: hostConfig: {
-            id = hostConfig.config.cookie.services.syncthing.runtimeId;
-            name = host;
-          }) (filterAttrs (host: hostConfig:
-            hostConfig.config.cookie.services.syncthing.runtimeId != null)
-            nodes);
-          untrackedHosts = {
-            phone = {
-              id =
-                "5UBCVLJ-TV7IVNG-CFTXONW-Z7YUSPJ-QCQSJC4-6T2BTAB-5BNNCD4-HETZSA7";
+      devices = let
+        trackedHosts = mapAttrs (host: hostConfig: {
+          id = hostConfig.config.cookie.services.syncthing.runtimeId;
+          name = host;
+        }) (filterAttrs (host: hostConfig:
+          hostConfig.config.cookie.services.syncthing.runtimeId != null) nodes);
+        untrackedHosts = {
+          phone = {
+            id =
+              "5UBCVLJ-TV7IVNG-CFTXONW-Z7YUSPJ-QCQSJC4-6T2BTAB-5BNNCD4-HETZSA7";
+          };
+        };
+      in untrackedHosts // trackedHosts;
+
+      folders = let
+        home = config.cookie.user.home;
+        def = {
+          "nixfiles" = {
+            path = "${home}/git/nixfiles";
+            devices = [ "cookiemonster" "thonkcookie" "pansear" ];
+            versioning = { # for the secrets
+              type = "simple";
+              params.keep = "10";
             };
           };
-        in untrackedHosts // trackedHosts;
 
-        folders = let
-          home = config.cookie.user.home;
-          def = {
-            "nixfiles" = {
-              path = "${home}/git/nixfiles";
-              devices = [ "cookiemonster" "thonkcookie" "pansear" ];
-              versioning = { # for the secrets
-                type = "simple";
-                params.keep = "10";
-              };
-            };
-
-            "sync" = {
-              path = "${home}/Sync";
-              devices = [ "cookiemonster" "thonkcookie" ];
-            };
-
-            "ssh" = {
-              path = "${home}/.ssh";
-              devices = [ "cookiemonster" "thonkcookie" ];
-              versioning = {
-                type = "simple";
-                params.keep =
-                  "50"; # keep 50 old versions of files around. should be fine considering keys are quite small.
-              };
-            };
-
-            "music" = {
-              id = "3ffxr-fpjwy"; # to keep compat with existing phone
-              path = "${home}/Music";
-              devices = [ "cookiemonster" "thonkcookie" "phone" ];
-            };
-
-            # it TURNS out it really doesn't like it when you do this.. for some reason the filenames incl the machine hostname
-            # "mail" = {
-            #   path = "${home}/Mail";
-            #   devices = [ "cookiemonster" "thonkcookie" ];
-            #   versioning = {
-            #     type = "trashcan";
-            #     params.cleanoutDays = "0"; # never. we can clean it up manually if needed, but this should be mostly write-only.
-            #   };
-            # };
+          "sync" = {
+            path = "${home}/Sync";
+            devices = [ "cookiemonster" "thonkcookie" ];
           };
-        in filterAttrs
-        (_: folder: any (d: config.networking.hostName == d) folder.devices)
-        def;
-      };
+
+          "ssh" = {
+            path = "${home}/.ssh";
+            devices = [ "cookiemonster" "thonkcookie" ];
+            versioning = {
+              type = "simple";
+              params.keep =
+                "50"; # keep 50 old versions of files around. should be fine considering keys are quite small.
+            };
+          };
+
+          "music" = {
+            id = "3ffxr-fpjwy"; # to keep compat with existing phone
+            path = "${home}/Music";
+            devices = [ "cookiemonster" "thonkcookie" "phone" ];
+          };
+
+          # it TURNS out it really doesn't like it when you do this.. for some reason the filenames incl the machine hostname
+          # "mail" = {
+          #   path = "${home}/Mail";
+          #   devices = [ "cookiemonster" "thonkcookie" ];
+          #   versioning = {
+          #     type = "trashcan";
+          #     params.cleanoutDays = "0"; # never. we can clean it up manually if needed, but this should be mostly write-only.
+          #   };
+          # };
+        };
+      in filterAttrs
+      (_: folder: any (d: config.networking.hostName == d) folder.devices) def;
     };
   };
 }
