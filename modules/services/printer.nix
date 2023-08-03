323d836dff963c1fe0fe66dd130c122177184749
ckie <git-525ff67@ckie.dev>
2023-08-03 17:39:16 +0300

    services/printer: try to workaround the upload bug,,

--- modules/services/printer.nix
+++ modules/services/printer.nix
@@ -3,6 +3,8 @@
 #     [crit] 1338328#1338328: *4166 open() "/tmp/nginx_client_body/0000000014" failed (2: No such file or directory), client: 192.168.0.29, server: print.atori, request: "POST /printers/RawDeskjet HTTP/1.1", host: "print.atori"
 # if so, go here:
 #     https://matrix.to/#/!KqkRjyTEzAGRiZFBYT:nixos.org/$hQsxz760br1mmnf9GlINF0c6OdWTd37y-cgXY4k9bho?via=nixos.org&via=matrix.org&via=tchncs.de
+# 2023-08-03 update:
+#     https://matrix.to/#/!KqkRjyTEzAGRiZFBYT:nixos.org/~!KqkRjyTEzAGRiZFBYT:nixos.org:m1691073056899.57?via=nixos.org&via=matrix.org&via=tchncs.de
 #
 { config, lib, pkgs, ... }:
 
@@ -76,6 +78,14 @@ in with lib; {
         common = {
           proxyPass = "http://[::1]:631";
           recommendedProxySettings = false;
+          extraConfig = ''
+            # Aug 03 15:35:26 pansear nginx[2036676]: 2023/08/03 15:35:26 [crit] 2036676#2036676: *3502 open() "/tmp/nginx_client_body/0000000025" failed (2: No such file or directory), client: 192.168.0.142, server: print.atori, request: "POST /printers/RawDeskjet HTTP/1.1", host: "print.atori:80"
+            # XXX: Attempt to workaround this by disabling some buffering things. Hopefully includes this nginx_client_body buffering.
+            # https://serverfault.com/a/733742
+            proxy_request_buffering off;
+            proxy_http_version 1.1;
+            client_max_body_size 0;
+          '';
         };
       in {
         ${cfg.host} = { locations."/" = common; };
