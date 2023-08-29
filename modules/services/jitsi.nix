da3008c28c650cc1f4633947682ebebc04f0c63f
ckie <git-525ff67@ckie.dev>
2023-08-29 19:02:42 +0300

    services/jitsi: continue bringup (very slowly)

--- modules/services/jitsi.nix
+++ modules/services/jitsi.nix
@@ -5,17 +5,26 @@ let cfg = config.cookie.services.jitsi;
 in with lib; {
   options.cookie.services.jitsi = {
     enable = mkEnableOption "Enables the Jitsi service";
+    host = mkOption {
+      type = types.nullOr types.str;
+      example = "jitsi.ckie.dev";
+      description = "host for the web interface";
+    };
   };
 
   config = mkIf cfg.enable {
     services.jitsi-meet = {
       enable = true;
       nginx.enable = true;
-      hostName = "jitsi.ckie.dev";
+      hostName = cfg.host;
       interfaceConfig = {
         SHOW_JITSI_WATERMARK = false;
         SHOW_WATERMARK_FOR_GUESTS = false;
       };
+    };
+
+    services.nginx.virtualHosts.${cfg.host} = {
+      enableACME = false;
     };
   };
 }
