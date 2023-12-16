18999a70b7a607a7d48d9dc6e4e40a15cad788f6
ckie <git-525ff67@ckie.dev>
2023-12-16 14:30:41 +0200

    home/dunst: autorestart (DISPLAY env race)

--- modules/home/dunst.nix
+++ modules/home/dunst.nix
@@ -13,50 +13,53 @@ in with lib; {
     enable = mkEnableOption "dunst notification daemon";
   };
 
-  config.services.dunst = mkIf cfg.enable {
-    enable = true;
-    package = pkgs.dunst_ckie;
-    settings = {
-      global = {
-        monitor = 0;
-        geometry = "300x100-30+20";
-        indicate_hidden = true;
-        shrink = false;
-        transperency = 0;
-        notification_height = 0;
-        seperator_height = 2;
-        padding = 8;
-        horizontal_padding = 8;
-        frame_width = 0;
-        seperator_color = "frame";
-        sort = true;
-        idle_threshold = 120;
-        font = "Monospace 10";
-        line_height = 0;
-        markup = "full";
-        format = "<b>%a</b>: %s\\n%b";
-        alignment = "center";
-        show_age_threshold = 60;
-        word_wrap = true;
-        ellipsize = "middle";
-        ignore_newline = false;
-        stack_duplicates = true;
-        hide_duplicate_count = false;
-        show_indicators = false;
-        icon_position = "off";
-      };
-      shortcuts = {
-        close = "ctrl+space";
-        close_all = "ctrl+shift+space";
-        history = "ctrl+grave";
-        context = "ctrl+shift+period";
-      };
-      urgency_low = normal_urgencies;
-      urgency_normal = normal_urgencies;
-      urgency_critical = {
-        background = "#ff1744";
-        foregrund = "#fffff";
-        timeout = 60;
+  config = mkIf cfg.enable {
+    systemd.user.services.dunst.Service.Restart = "always";
+    services.dunst = {
+      enable = true;
+      package = pkgs.dunst_ckie;
+      settings = {
+        global = {
+          monitor = 0;
+          geometry = "300x100-30+20";
+          indicate_hidden = true;
+          shrink = false;
+          transperency = 0;
+          notification_height = 0;
+          seperator_height = 2;
+          padding = 8;
+          horizontal_padding = 8;
+          frame_width = 0;
+          seperator_color = "frame";
+          sort = true;
+          idle_threshold = 120;
+          font = "Monospace 10";
+          line_height = 0;
+          markup = "full";
+          format = "<b>%a</b>: %s\\n%b";
+          alignment = "center";
+          show_age_threshold = 60;
+          word_wrap = true;
+          ellipsize = "middle";
+          ignore_newline = false;
+          stack_duplicates = true;
+          hide_duplicate_count = false;
+          show_indicators = false;
+          icon_position = "off";
+        };
+        shortcuts = {
+          close = "ctrl+space";
+          close_all = "ctrl+shift+space";
+          history = "ctrl+grave";
+          context = "ctrl+shift+period";
+        };
+        urgency_low = normal_urgencies;
+        urgency_normal = normal_urgencies;
+        urgency_critical = {
+          background = "#ff1744";
+          foregrund = "#fffff";
+          timeout = 60;
+        };
       };
     };
   };
