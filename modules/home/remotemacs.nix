caa188a1258fd84da6eec3cab0c099a35a0ceba8
ckie <git-525ff67@ckie.dev>
2024-01-21 15:48:02 +0200

    home/remotemacs: alacritty migrated to toml

--- modules/home/remotemacs.nix
+++ modules/home/remotemacs.nix
@@ -6,12 +6,12 @@ let
   cfg = config.cookie.remotemacs;
   targetHost = "cookiemonster";
   # YAML is a superset of JSON now, so this is okay (:
-  termConfig = pkgs.writeText "alacritty.yml" (builtins.toJSON {
+  termConfig = (pkgs.formats.toml {}).generate "alacritty.toml" {
     window.dimensions = {
       columns = 174;
       lines = 46;
     };
-  });
+  };
   wrapper = pkgs.writeShellScriptBin "remotemacs" ''
     # the unset works around https://github.com/mobile-shell/mosh/issues/1134
     ${pkgs.alacritty}/bin/alacritty --config-file ${termConfig} -t 'Emacs-over-ssh' -e ssh ${targetHost} -t "unset SSH_TTY && emacsclient -nw"
