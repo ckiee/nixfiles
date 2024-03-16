abd67616407af00e9d783bc65569607df91fe88b
ckie <git-525ff67@ckie.dev>
2024-03-16 18:44:39 +0200

    wivrn: use pr

--- modules/wivrn/default.nix
+++ modules/wivrn/default.nix
@@ -5,15 +5,20 @@ let cfg = config.cookie.wivrn;
 in with lib; {
   options.cookie.wivrn = { enable = mkEnableOption "wivrn"; };
 
-  # FIXME: Move this. This is only for testing until we package WiVRn as a
-  # nixpkgs NixOS module.
   config = mkIf cfg.enable {
-    services.avahi = mkIf cfg.enable {
+    # services.avahi = mkIf cfg.enable {
+    #   enable = true;
+    #   publish = {
+    #     enable = true;
+    #     userServices = true;
+    #   };
+    # };
+    #
+    # Above comment for wivrn dev, but now there's a NixOS module:
+    services.wivrn = {
       enable = true;
-      publish = {
-        enable = true;
-        userServices = true;
-      };
+      highPriority = true;
+      defaultRuntime = true;
     };
   };
 }
