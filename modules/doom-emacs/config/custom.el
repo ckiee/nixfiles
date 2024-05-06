6dc4c54915d1cd8abb4c714341405c0c3c617b09
ckie <git-525ff67@ckie.dev>
2024-05-06 05:07:47 +0300

    doom-emacs: it wants

--- modules/doom-emacs/config/custom.el
+++ modules/doom-emacs/config/custom.el
@@ -4,7 +4,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(safe-local-variable-values '((eval setq-local org-duration-format 'h:mm))))
+ '(safe-local-variable-values
+   '((eval c-set-offset 'inlambda 0)
+     (eval c-set-offset 'access-label '-)
+     (eval c-set-offset 'substatement-open 0)
+     (eval c-set-offset 'arglist-cont-nonempty '+)
+     (eval c-set-offset 'arglist-cont 0)
+     (eval c-set-offset 'arglist-intro '+)
+     (eval c-set-offset 'inline-open 0)
+     (eval c-set-offset 'defun-open 0)
+     (eval c-set-offset 'innamespace 0)
+     (indicate-empty-lines . t)
+     (c-block-comment-prefix . "  ")
+     (eval setq-local org-duration-format 'h:mm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
