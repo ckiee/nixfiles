d7256131a31212e533d0dab432df3bb30b99ec8b
ckie <git-525ff67@ckie.dev>
2024-06-30 15:48:22 +0300

    blep

--- deploy/repl.nix
+++ deploy/repl.nix
@@ -1,7 +1,9 @@
 let
   sources = import ../nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-in pkgs.lib // rec {
+in
+
+pkgs.lib // rec {
   inherit pkgs sources;
   eval = import "${sources.morph}/data/eval-machines.nix" {
     networkExpr = ../deploy/morph.nix;
