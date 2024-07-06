dd214b49100a2b5fedc2f4d6692cc39ac3bb5d87
ckie <git-525ff67@ckie.dev>
2024-07-06 03:40:30 +0300

    services/pupcat: use stfed to restart pupcat on change

--- modules/services/pupcat.nix
+++ modules/services/pupcat.nix
@@ -16,11 +16,30 @@ in with lib; {
       };
 
       systemd.services.pupcat.serviceConfig = {
-        ReadWritePaths = [ "/var/www/websync/mei.puppycat.house/www" ];
+        ReadWritePaths = [ "/var/www/websync/mei.puppycat.house" ];
         SystemCallFilter = mkForce [ ];
       };
 
       users.users.pupcat.extraGroups = [ "websync" ];
+
+      cookie.services.stfed = {
+        enable = true;
+        hooks = [{
+          folder = "${config.cookie.user.home}/www/mei.puppycat.house";
+          # file_down_sync_done: triggers when a file has been fully synchronized locally (see filter to match for a specific file)
+          # folder_down_sync_done: triggers when a folder has been fully synchronized locally
+          # file_conflict: triggers when Syncthing creates a .stconflict file due to a synchronization conflict
+          event = "folder_down_sync_done";
+          # glob rule for specific file matching for file_down_sync_done events
+          # filter = "shopping-list.txt"
+          command = pkgs.writeShellScript "ppcat-restart-cmd" ''
+            set -euxo pipefail
+            sleep 5
+            /run/wrappers/bin/sudo systemctl restart pupcat
+          '';
+          allow_concurrent = false;
+        }];
+      };
     }
 
     (util.mkService "pupcat" {
@@ -32,7 +51,7 @@ in with lib; {
       # };
       script = ''
         HOST=127.0.0.1 PORT=32582 ORIGIN=https://mei.puppycat.house exec \
-            ${pkgs.bun}/bin/bun /var/www/websync/mei.puppycat.house/www.stage/index.js
+            ${pkgs.bun}/bin/bun /var/www/websync/mei.puppycat.house/www/index.js
       '';
     })
   ]);
