e2f16abbb6452f9a1ba6ade06e678f06f2702a10
ckie <git-525ff67@ckie.dev>
2025-03-23 11:19:53 +0200

    services/hedgedoc: notes

--- modules/services/hedgedoc/default.nix
+++ modules/services/hedgedoc/default.nix
@@ -14,6 +14,10 @@ in with lib; {
 
   config = mkIf cfg.enable {
     systemd.services.hedgedoc.after = [ "postgresql.service" ];
+
+    # (ssh) ckie@flowe ~ -> sudo cp /run/hedgedoc/config.json /tmp/
+    # (ssh) ckie@flowe ~ -> sudo chmod 444 /tmp/config.json
+    # (ssh) ckie@flowe ~ -> sudo -u hedgedoc NODE_ENV=production CMD_CONFIG_FILE=/tmp/config.json /nix/store/vqv3dblqx76k185jg4ym5i3dz196lv19-nodejs-20.18.1/bin/node /nix/store/x00v8pqc4iy68b7kcvii5qhf97rxs424-hedgedoc-1.10.0/share/hedgedoc/bin/manage_users --add mei@puppycat.house --pass awawa
     services.hedgedoc = {
       enable = true;
       settings = {
