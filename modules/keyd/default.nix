60227fa014eb7d30e50e78723e584cf6bcf552f2
ckie <git-525ff67@ckie.dev>
2025-05-18 00:18:26 +0300

    keyd: warpd, blabla

--- modules/keyd/default.nix
+++ modules/keyd/default.nix
@@ -1,18 +1,41 @@
 { lib, config, pkgs, ... }:
 
-let cfg = config.cookie.keyd;
+with lib;
 
-in with lib; {
-  options.cookie.keyd = { enable = mkEnableOption "keyd"; };
+let
+  cfg = config.cookie.keyd;
+  cmd = line: name: # NOTE -u forces single-instance.
+    "command(systemd-run -u ${name} --collect --wait --user --machine ckie@.host ${line})";
+in {
+  options.cookie.keyd = { enable = mkEnableOption "keyd & warpd"; };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ keyd ];
+    environment.systemPackages = with pkgs; [ keyd warpd ];
+
+    home-manager.users.ckie = { config, ... }: {
+      xdg.configFile."warpd/config".source = ./warpd.cfg;
+    };
+
     services.keyd = {
       enable = true;
       keyboards = {
         gk600 = {
           ids = [ "0db0:ea47" ];
-          settings = { "shift+meta" = { f23 = "rightcontrol"; }; };
+          settings = {
+            main = {
+              # "copilot" key
+              "leftmeta+leftshift+f23" = "overload(righty, rightcontrol)";
+            };
+
+            righty = rec {
+              "m" = cmd "${pkgs.warpd}/bin/warpd --hint2" "keyd-warpd";
+              "n" = m;
+              "space+m" = m;
+              "space+n" = m;
+
+              space = cmd "${pkgs.warpd}/bin/warpd --normal" "keyd-warpd";
+            };
+          };
         };
       };
     };
